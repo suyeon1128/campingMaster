44e9eab20a1834bba28225c41b91206b407eafc6
icekimchi <pjh010828@gmail.com>
2024-04-18 23:19:57 +0900

    [FEAT] 스플래시 화면 구현

--- app/src/main/java/com/example/campingmaster/MainActivity.java
+++ app/src/main/java/com/example/campingmaster/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.campingmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Handle the splash screen transition.
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
